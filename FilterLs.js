616bd3a27b0d96574ca6d8e869c451c922103f62
keomony khun <keomony@gmail.com>
2017-01-31 16:04:48 +0000

    Rerfactors previous commit by using forEach

--- FilterLs.js
+++ FilterLs.js
@@ -1,13 +1,13 @@
 var fs = require('fs')
 var path = require('path')
 var name = process.argv[2]
-var ext = process.argv[3]
+var ext = "." + process.argv[3]
 
 fs.readdir(name, function(err, list) {
   if(err) throw err
-  for(i = 0; i < list.length; i++) {
-    if(path.extname(list[i]) === '.' + ext) {
-      console.log(list[i]);
-    } 
-  }
+  list.forEach(function(l){
+    if(path.extname(l) === ext) {
+      console.log(l);
+    }
+  })
 })
