1792fe544940bdfdfc887a785abd891508396ec8
keomony khun <keomony@gmail.com>
2017-01-31 21:52:47 +0000

    Refactors previous commit

--- HttpClient.js
+++ HttpClient.js
@@ -2,7 +2,7 @@ var http = require('http')
 var url = process.argv[2]
 
 http.get(url, function(response){
-  response.setEncoding("utf8").on("data", function(data){
-    console.log(data)
-  })
-})
+  response.setEncoding("utf8")
+  response.on("data", console.log)
+  response.on("error", console.log)
+}).on("error", console.log)
