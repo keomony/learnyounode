d3b26c0cc856ce375226ea0e1add83646337dfbc
keomony khun <keomony@gmail.com>
2017-01-31 22:32:41 +0000

    Refactors the previous commit by using official code

--- HttpCollect.js
+++ HttpCollect.js
@@ -2,11 +2,13 @@ var http = require('http')
 var bl = require('bl')
 var url = process.argv[2]
 
-http.get(url, function(response){
-  response.on("error", console.error)
-  response.setEncoding("utf8")
-  response.pipe(bl(function(err,data){
-    console.log(data.length)
-    console.log(data.toString())
-  }))
-}).on("error", console.error)
+   http.get(url, function (response) {
+     response.pipe(bl(function (err, data) {
+       if (err) {
+         return console.error(err)
+       }
+       data = data.toString()
+       console.log(data.length)
+       console.log(data)
+     }))
+   })
